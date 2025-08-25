1efe4e7cccf980e7974988449b79bbd57e527e54
Diego Pulig <dapj06@gmail.com>
2025-08-24 19:50:06 -0500

    Add files via upload

---
+++ tareasemana8for/src/main/java/com/mycompany/tareasemana8for/Tareasemana8for.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareasemana8for;
+
+/**
+ *
+ * @author World
+ */
+public class Tareasemana8for {
+
+    public static void main(String[] args) {
+       int suma=0;
+        for (int i=1; i<=10;i++){
+            suma=suma+i;
+            System.out.println(i);
+        }
+        System.out.println("el valor de la suma total es:" +suma);
+    }
+}
